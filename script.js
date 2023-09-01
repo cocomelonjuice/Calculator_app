42c9f3314913781ad86571a721192e4ef228aeea
cocomelonjuice <cocomelonjuice293@gmail.com>
2023-09-01 14:04:03 +0700

    new

---
+++ script.js
@@ -0,0 +1,39 @@
+let a = 0;
+let b = 0;
+let x = 0;
+
+//nhập a
+// chọn operator + - x /
+// nhập b
+// nhấn = 
+
+/* 
+hàm để thực hiện tính toán
+
+const Calculate = () =>{
+
+if (operator = "+"){
+    return x = a+b;
+}
+else if (operator = "-"){
+    return x = a-b;
+}
+else if (operator = "x"){
+    return x = axb;
+}
+else if (operator = "/"){
+    return x = a/b;
+}
+}
+
+hàm nhập các nút từ bàn phím: a,b, operator, dấu chấm số thập phân...
+hàm hiển thị kết quả 
+hàm xóa toàn bộ
+hàm xóa từng ký tự a, operator, b
+hàm thực hiện tính tiếp sau khi đã tính toán 1,2,3...n lần
+
+
+
+
+
+
